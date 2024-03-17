16162d81a60209aec45155c8699a465b99ab999f
BBOXEEEE <noeyhesx@naver.com>
2024-03-17 17:11:08 +0900

    feat: 받침 읽기 기능 추가

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ConsonantReader from './src/screens/reader/ConsonantReader';
 import VowelReader from './src/screens/reader/VowelReader';
+import FinalConsonantReader from './src/screens/reader/FinalConsonantReader';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <VowelReader />
+      <FinalConsonantReader />
       <StatusBar style="auto" />
     </View>
   );
