aa5e5f89c7ed01575f2c245db5b10caa11c5da59
BBOXEEEE <noeyhesx@naver.com>
2024-03-19 20:03:53 +0900

    refactor: 점자 리스트 상수로 분리

--- src/screens/reader/InitialConsonantReader.js
+++ src/screens/reader/InitialConsonantReader.js
@@ -1,27 +1,10 @@
 import BrailleReader from '../../components/BrailleReader';
+import BrailleList from '../../constants/BrailleList';
 
-const category = '자음';
-const brailleSimbols = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅍ', 'ㅌ', 'ㅎ', 'ㄲ', 'ㄸ', 'ㅃ', 'ㅆ', 'ㅉ']
-const brailleList = [
-    [0, 0, 0, 1, 0, 0], // ㄱ
-    [1, 0, 0, 1, 0, 0], // ㄴ
-    [0, 1, 0, 1, 0, 0], // ㄷ
-    [0, 0, 0, 0, 1, 0], // ㄹ
-    [1, 0, 0, 0, 1, 0], // ㅁ
-    [0, 0, 0, 1, 1, 0], // ㅂ
-    [0, 0, 0, 0, 0, 1], // ㅅ
-    [0, 0, 0, 1, 0, 1], // ㅈ
-    [0, 0, 0, 0, 1, 1], // ㅊ
-    [1, 1, 0, 1, 0, 0], // ㅋ
-    [1, 1, 0, 0, 1, 0], // ㅍ
-    [1, 0, 0, 1, 1, 0], // ㅌ
-    [0, 1, 0, 1, 1, 0], // ㅎ
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0], // ㄲ
-    [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0], // ㄸ
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0], // ㅃ
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1], // ㅆ
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1]  // ㅉ
-]
+const initialConsonant = BrailleList.INITIAL_CONSONANT;
+const category = initialConsonant.category;;
+const brailleSimbols = initialConsonant.symbols;
+const brailleList = initialConsonant.braille;
 
 const InitialConsonantReader = () => {
     return <BrailleReader category={category} brailleSymbols={brailleSimbols} brailleList={brailleList} />;
